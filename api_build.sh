5d5528610676f83073adbb9d11080bae5facd9e6
Andras Bubics <abubics@dius.com.au>
2017-04-24 16:16:46 +1000

    Remove Pact creds passthrough for api_build.

--- api_build.sh
+++ api_build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -eu
 
 docker run --rm \
-  -e pactBrokerUsername=$pactBrokerUsername \
-  -e pactBrokerPassword=$pactBrokerPassword \
-  -e pactBrokerAccount=$pactBrokerAccount \
   -v "$PWD/app":/usr/src/provider \
   -w /usr/src/provider \
   openjdk:8 \
